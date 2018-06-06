7093ad730a2f4da6e37a14733ac78cb7f5f2daed
Vic <305880887@qq.com>
2018-06-06 14:53:05 +0800

    add video

--- pages/examples/index.js
+++ pages/examples/index.js
@@ -25,7 +25,7 @@
 
 开发环境：https://code.leaplearner.com/
 更多例子：https://code.leaplearner.com/show.html?v=11511
-目前最新库版本：http://llcs-1252287760.cossh.myqcloud.com/lleg/lleg.min.v0.2.4.js
+目前最新库版本：https://rss.leaplearner.com/teachfiles/Text/lleg.min.v.0.2.7.js
 
 ***************************************************************/
 
